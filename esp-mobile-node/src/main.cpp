6653203917b55308d2d94146cc4a07baceed52cf
EcoFreshKase <alexander.jablo@gmail.com>
2025-03-21 14:35:00 +0100

    setup connection to esp-bounce-back

--- esp-mobile-node/src/main.cpp
+++ esp-mobile-node/src/main.cpp
@@ -9,20 +9,27 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   Serial.begin(115200);
-  SerialBT.begin("esp-mobile-node");
-  Serial.println("Device is ready for connection.");
+  SerialBT.begin("esp-mobile-node", true);
+  
+  // Setup connection to bounce back controller
+  if (!SerialBT.connect("esp-bounce-back")) {
+    Serial.println("Failed to connect to bounce back controller.");
+    abort();
+  };
+  Serial.println("Connected to bounce back controller.");
+
+  Serial.println("Device is ready for action.");
+
 }
 
 void loop() {
   boolean buttonState = digitalRead(BUTTON_PIN) == LOW;
   if (!lastButtonState && buttonState) {
     Serial.print("Button is pressed.");
+    SerialBT.write(97);
   };
   lastButtonState = buttonState;
 
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
   if (SerialBT.available()) {
     Serial.write(SerialBT.read());
   }
