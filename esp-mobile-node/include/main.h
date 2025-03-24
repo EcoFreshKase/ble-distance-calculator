ce8b4f019a928afcf58b8eec927e7457bec4168f
EcoFreshKase <alexander.jablo@gmail.com>
2025-03-24 15:37:09 +0100

    init experiment

--- esp-mobile-node/include/main.h
+++ esp-mobile-node/include/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-const char DEVICE_NAME_ESP_BOUNCE_BACK[] = "ESP_BOUNCE_BACK";
+const char DEVICE_NAME_ESP_BOUNCE_BACK[] = "esp-bounce-back";
+const int BUTTON_PIN = 18;
+const char DEVICE_NAME[] = "esp-mobile-node";
+
+const int EXPERIMENT_NUM_SIGNALS = 100;
 
 #endif
--- esp-mobile-node/src/main.cpp
+++ esp-mobile-node/src/main.cpp
@@ -3,25 +3,21 @@
 #include "main.h"
 
 BluetoothSerial SerialBT;
-int BUTTON_PIN = 18;
 boolean lastButtonState = false;
 boolean connected = false;
 
-boolean connectToBounceBackController() {
-  Serial.println("Connecting to bounce back controller....");
-  if (!SerialBT.connect(DEVICE_NAME_ESP_BOUNCE_BACK)) {
-    Serial.println("Failed to connect to bounce back controller.");
-    return false;
-  };
-  Serial.println("Connected to bounce back controller.");
-  return true;
-}
+boolean experimentOngoing = false;
+int experimentCounter = 0;
+boolean experimentReceiving = false;
+unsigned long experimentStartTime = 0;
+
+boolean connectToBounceBackController();
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   Serial.begin(115200);
-  SerialBT.begin("esp-mobile-node", true);
+  SerialBT.begin(DEVICE_NAME, true);
   
   // Setup connection to bounce back controller
   unsigned long startTime = millis();
@@ -33,7 +29,7 @@ void setup() {
   if (!connected) {
     Serial.println("Failed to connect to bounce back controller. Restarting device....");
     ESP.restart();
-  }  
+  }
 
   Serial.println("Device is ready for action.");
 }
@@ -41,13 +37,47 @@ void setup() {
 void loop() {
   boolean buttonState = digitalRead(BUTTON_PIN) == LOW;
   if (!lastButtonState && buttonState) {
-    Serial.print("Button is pressed.");
-    SerialBT.write(97);
+    Serial.println("Starting Experiment.");
+    experimentOngoing = true;
+    experimentStartTime = millis();
   };
   lastButtonState = buttonState;
 
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
+  // Experiment Section
+  if (experimentOngoing) {
+
+    // Is device currently waiting for a response
+    if (experimentReceiving) {
+      if (SerialBT.available()) {
+        SerialBT.read();
+
+        experimentCounter++;
+        experimentReceiving = false;
+      }
+    } else {
+      SerialBT.write(experimentCounter);
+      experimentReceiving = true;
+      experimentCounter++;
+    }
+
+    // End experiment
+    if (experimentCounter > EXPERIMENT_NUM_SIGNALS) {
+      Serial.printf("Experiment completed after %dms.\n", millis() - experimentStartTime);
+      experimentOngoing = false;
+      experimentReceiving = false;
+      experimentCounter = 0;
+      SerialBT.flush();
+    }
   }
   delay(25);
+}
+
+boolean connectToBounceBackController() {
+  Serial.println("Connecting to bounce back controller....");
+  if (!SerialBT.connect(DEVICE_NAME_ESP_BOUNCE_BACK)) {
+    Serial.println("Failed to connect to bounce back controller.");
+    return false;
+  };
+  Serial.println("Connected to bounce back controller.");
+  return true;
 }
