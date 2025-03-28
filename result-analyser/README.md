4e04df3d012af5b3bfeadf33350dd2ae063bcb44
EcoFreshKase <alexander.jablo@gmail.com>
2025-03-28 13:27:58 +0100

    add analysis for multiple datasets at a time

---
+++ result-analyser/README.md
@@ -0,0 +1,5 @@
+## Experiments
+
+Experiments 1 and 2 were made with 1000 bounce backs at a distance of 20cm.
+
+Experiment 3 was made with 1000 bounce backs at a distance of multiple meters (~4m)
--- result-analyser/main.py
+++ result-analyser/main.py
@@ -2,25 +2,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 from math import sqrt
+from typing import Dict
 
-csv_path = os.path.join("resources", "experiment_2.csv")
+csv_path_1 = os.path.join("resources", "experiment_2.csv")
+csv_path_2 = os.path.join("resources", "experiment_3.csv")
 measurement_device_deviation = 1 # ms
 
-df = pd.read_csv(csv_path)
+def calcDeviation(csv_path) -> Dict[str, float]:
+    df = pd.read_csv(csv_path)
 
-duration = df['end_time'] - df["start_time"]
+    duration = df['end_time'] - df["start_time"]
 
-linear_deviation = duration - duration.mean()
-quadratic_deviation = linear_deviation**2
+    linear_deviation = duration - duration.mean()
+    quadratic_deviation = linear_deviation**2
 
 
-e_z = sum(quadratic_deviation / (len(duration) * (len(duration) - 1)))
-u = sqrt(measurement_device_deviation**2 + e_z)
+    e_z = sum(quadratic_deviation / (len(duration) * (len(duration) - 1)))
+    u = sqrt(measurement_device_deviation**2 + e_z)
+    return {
+        "mean": duration.mean(),
+        "deviation": u,
+        "df": df,
+        "durationData": duration
+    }
 
-print(f"Measurement result: {duration.mean()} +/- {u} ms")
+res1 = calcDeviation(csv_path_1)
+res2 = calcDeviation(csv_path_2)
+
+print(f"Measurement result 1: {res1["mean"]} +/- {res1["deviation"]} ms")
+print(f"Measurement result 2: {res2["mean"]} +/- {res2["deviation"]} ms")
 
 plt.figure(figsize=(10, 6))
-plt.plot(df['id'], duration / 1000, marker='o', linestyle='None', color='b', label='Duration')
+plt.plot(res1["df"]['id'], res1["durationData"] / 1000, marker='o', linestyle='None', color='b', label='res1 Data')
+plt.plot(res2["df"]['id'], res2["durationData"] / 1000, marker='o', linestyle='None', color='r', label='res2 Data')
 plt.xlabel('ID')
 plt.ylabel('Duration in s')
 plt.title('Duration in s over time')
