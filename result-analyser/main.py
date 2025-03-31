575fbea0412757883d88e7197ade174db71e8475
EcoFreshKase <alexander.jablo@gmail.com>
2025-03-31 16:49:07 +0200

    add distance calculation

--- result-analyser/main.py
+++ result-analyser/main.py
@@ -3,13 +3,24 @@ import matplotlib.pyplot as plt
 import os
 import argparse
 from math import sqrt
-from typing import Dict
-from matplotlib import cm
+from typing import Dict, TypedDict
+from matplotlib import colormaps
 
-def calcDeviation(csv_path: str, measurement_device_deviation: int) -> Dict[str, float]:
+class DeviatedResult(TypedDict):
+    mean: float
+    deviation: float
+    df: pd.DataFrame
+    durationData: pd.Series
+    numOfBounceBack: int
+
+class DeviatedDistance(TypedDict):
+    distance: float
+    deviation: float
+
+def calcDeviation(csv_path: str, measurement_device_deviation: int, tcn: float) -> DeviatedResult:
     df = pd.read_csv(csv_path)
 
-    duration = df['end_time'] - df["start_time"]
+    duration = df['end_time'] - df["start_time"] - tcn
 
     linear_deviation = duration - duration.mean()
     quadratic_deviation = linear_deviation**2
@@ -21,11 +32,20 @@ def calcDeviation(csv_path: str, measurement_device_deviation: int) -> Dict[str,
         "mean": duration.mean(),
         "deviation": u,
         "df": df,
-        "durationData": duration
+        "durationData": duration,
+        "numOfBounceBack": df['num_of_passes'][0],
     }
 
 def plotDataSet(data_set: pd.DataFrame, color="b", title='data'):
     plt.plot(data_set['id'], (data_set['end_time'] - data_set['start_time']) / 1000, marker='o', linestyle='None', color=color, label=title)
+
+def calculateDistance(deviatedResult: DeviatedResult, numOfBounceBacks: int) -> DeviatedDistance:
+    speedOfLight = 299792.458 # m/ms
+    getDistance = lambda t: (t * speedOfLight) / (2 * numOfBounceBacks) # in m
+    return {
+        "distance": getDistance(deviatedResult["mean"]),
+        "deviation": getDistance(deviatedResult["deviation"]),
+    }
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Processes distance measurement data.')
@@ -34,16 +54,19 @@ if __name__ == "__main__":
     parser.add_argument('--plot', action='store_true', help='Plot the data')
 
     args = parser.parse_args()
-    measurement_device_deviation = args.device_deviation # ms
 
-    results = [(calcDeviation(csv_path, measurement_device_deviation), csv_path) for csv_path in args.csv_path]
+    measurement_device_deviation = args.device_deviation  # ms
+
+    results = [(calcDeviation(csv_path, measurement_device_deviation, 0), csv_path) for csv_path in args.csv_path]
 
     for res, csv_path in results:
         print(f"{os.path.basename(csv_path)}: {res['mean']} +/- {res['deviation']} ms")
+        distance = calculateDistance(res, res['numOfBounceBack'])
+        print(f"{os.path.basename(csv_path)}: {distance['distance']} +/- {distance['deviation']} mm")
 
     if args.plot:
         plt.figure(figsize=(10, 6))
-        colorMap = cm.get_cmap('viridis', len(results))
+        colorMap = colormaps['viridis']
         for i, (res, csv_path) in enumerate(results):
             color = colorMap(i / len(results))
             plotDataSet(res["df"], color=color, title=os.path.basename(csv_path))
