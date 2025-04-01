f35f3f5826b43892f640a5058bc142fdd4b7a535
EcoFreshKase <alexander.jablo@gmail.com>
2025-04-01 11:25:48 +0200

    implement tcn calculation

--- result-analyser/README.md
+++ result-analyser/README.md
@@ -5,3 +5,5 @@ Experiments 1 and 2 were made with 1000 bounce backs at a distance of 20cm.
 Experiment 3 was made with 1000 bounce backs at a distance of multiple meters (~4m)
 
 Experiment 4 was made (in the office) with 1000 bounce backs at a distance of around 20cm.
+
+Experiment 5 was made (in the office) with 1000 bounce backs at a distance of 1m.
--- result-analyser/main.py
+++ result-analyser/main.py
@@ -6,6 +6,8 @@ from math import sqrt
 from typing import Dict, TypedDict
 from matplotlib import colormaps
 
+speedOfLight = 299792.458 # m/ms
+
 class DeviatedResult(TypedDict):
     mean: float
     deviation: float
@@ -17,10 +19,10 @@ class DeviatedDistance(TypedDict):
     distance: float
     deviation: float
 
-def calcDeviation(csv_path: str, measurement_device_deviation: int, tcn: float) -> DeviatedResult:
+def calcDeviation(csv_path: str, measurement_device_deviation: int, offset=0) -> DeviatedResult:
     df = pd.read_csv(csv_path)
 
-    duration = df['end_time'] - df["start_time"] - tcn
+    duration = df['end_time'] - df["start_time"] - offset
 
     linear_deviation = duration - duration.mean()
     quadratic_deviation = linear_deviation**2
@@ -40,24 +42,35 @@ def plotDataSet(data_set: pd.DataFrame, color="b", title='data'):
     plt.plot(data_set['id'], (data_set['end_time'] - data_set['start_time']) / 1000, marker='o', linestyle='None', color=color, label=title)
 
 def calculateDistance(deviatedResult: DeviatedResult, numOfBounceBacks: int) -> DeviatedDistance:
-    speedOfLight = 299792.458 # m/ms
     getDistance = lambda t: (t * speedOfLight) / (2 * numOfBounceBacks) # in m
     return {
         "distance": getDistance(deviatedResult["mean"]),
         "deviation": getDistance(deviatedResult["deviation"]),
     }
 
+# measurement_device_deviation in ms and tcn_distance in mm
+def calcTcn(csv_path: str, measurement_device_deviation: int, tcn_distance: int) -> DeviatedResult:
+    df = pd.read_csv(csv_path)
+    tcn = calcDeviation(csv_path, measurement_device_deviation, offset=2 * int(df['num_of_passes'][0]) * tcn_distance / speedOfLight)
+    return tcn
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Processes distance measurement data.')
     parser.add_argument('csv_path', type=str, nargs='+', help='Paths to the CSV files')
     parser.add_argument('--device_deviation', '-d', required=True, type=int, help='Measurement device deviation in ms')
     parser.add_argument('--plot', action='store_true', help='Plot the data')
+    parser.add_argument('--tcn', type=str, nargs=2, metavar=('CSV_PATH', 'DISTANCE'), required=True,
+                        help='TCN arguments. Takes the path to the csv file containing the measurement data to calculate tcn and an the distance of the tcn measurement in millimeters.')
 
     args = parser.parse_args()
 
-    measurement_device_deviation = args.device_deviation  # ms
+    tcn_csv_path, tcn_distance = args.tcn
+    measurement_device_deviation = args.device_deviation # ms
 
-    results = [(calcDeviation(csv_path, measurement_device_deviation, 0), csv_path) for csv_path in args.csv_path]
+    tcn = calcTcn(tcn_csv_path, measurement_device_deviation, int(tcn_distance))
+    print(f"TCN: {tcn['mean']} +/- {tcn['deviation']} ms")
+
+    results = [(calcDeviation(csv_path, measurement_device_deviation, tcn['mean']), csv_path) for csv_path in args.csv_path]
 
     for res, csv_path in results:
         print(f"{os.path.basename(csv_path)}: {res['mean']} +/- {res['deviation']} ms")
@@ -78,4 +91,4 @@ if __name__ == "__main__":
         plt.show()
 
 # cur command:
-# -d 1 --plot resources/experiment_2.csv resources/experiment_3.csv resources/experiment_4.csv
+# -d 1 --plot --tcn resources/experiment_5.csv 1000 resources/experiment_3.csv resources/experiment_5.csv
